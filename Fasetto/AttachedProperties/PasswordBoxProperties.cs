0b5c78b53cfc0b251ab2cac7c897b253a6c67f72
Hexin <hexin.email@qq.com>
2024-08-18 23:28:40 +0800

    给PasswordBox添加HasPassword附加属性

---
+++ Fasetto/AttachedProperties/PasswordBoxProperties.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace Fasetto.AttachedProperties;
+
+public class PasswordBoxProperties
+{
+    public static readonly DependencyProperty HasPasswordProperty =
+        DependencyProperty.RegisterAttached("HasPassword", typeof(bool), typeof(PasswordBoxProperties), new PropertyMetadata(false));
+
+    public static bool GetHasPassword(DependencyObject obj)
+    {
+        return (bool)obj.GetValue(HasPasswordProperty);
+    }
+
+    public static void SetHasPassword(DependencyObject obj, bool value)
+    {
+        obj.SetValue(HasPasswordProperty, value);
+    }
+}
---
+++ Fasetto/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fasetto.ViewModels;
+
+public partial class LoginPageViewModel : ObservableObject
+{
+    private SecureString? _securePassword;
+
+    public SecureString? SecurePassword
+    {
+        private get { return _securePassword; }
+        set
+        {
+            _securePassword = value;
+            if (_securePassword != null && _securePassword.Length > 0)
+            {
+                HasPassword = true;
+            }
+            else
+            {
+                HasPassword = false;
+            }
+        }
+    }
+
+    [ObservableProperty]
+    private bool _hasPassword = false;
+}
