9a23a2bada71e2e2bc94b7fc7f70acc49ac3b02d
Hexin <hexin.email@qq.com>
2024-09-12 13:24:18 +0800

    移动MainWindow

---
+++ Fasetto/Views/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Fasetto.Helpers;
+using Fasetto.ViewModels;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Fasetto.Views;
+
+/// <summary>
+/// MainWindow.xaml 的交互逻辑
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        DataContext = new MainWindowViewModel();
+        var resizer = new WindowResizer(this);
+    }
+
+    private void CloseButton_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void IconButton_click(object sender, RoutedEventArgs e)
+    {
+        SystemCommands.ShowSystemMenu(this, GetMousePosition());
+    }
+
+    /// <summary>
+    /// 获取鼠标当前坐标
+    /// </summary>
+    /// <returns></returns>
+    private Point GetMousePosition()
+    {
+        var postion = Mouse.GetPosition(this);
+        if (this.WindowState == WindowState.Maximized)
+            return postion;
+
+        return new Point(postion.X + this.Left, postion.Y + this.Top);
+    }
+}
