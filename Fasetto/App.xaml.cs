390089573afb4c002dd651599b7529af6caa7766
Hexin <hexin.email@qq.com>
2024-10-01 00:13:20 +0800

    添加依赖注入容器

--- Fasetto/App.xaml.cs
+++ Fasetto/App.xaml.cs
@@ -1,14 +1,23 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
 
-namespace Fasetto
+namespace Fasetto;
+
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public new static App Current = (App)Application.Current;
+
+    public IServiceProvider Services { get; }
+
+    public App()
     {
+        Services = ConfigureServices();
+    }
+
+    private static IServiceProvider ConfigureServices()
+    {
+        var services = new ServiceCollection();
+        return services.BuildServiceProvider();
     }
 
 }
