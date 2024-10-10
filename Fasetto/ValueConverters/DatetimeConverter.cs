f4c23d19640cb538dd937f675cf22be95ba1e859
Hexin <hexin.email@qq.com>
2024-10-10 22:38:18 +0800

    添加DateTime值转换器

---
+++ Fasetto/ValueConverters/DatetimeConverter.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace Fasetto.ValueConverters;
+
+public class DatetimeConverter : BaseValueConverter<DatetimeConverter>
+{
+    public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return ((DateTime)value).ToString();
+    }
+
+    public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
