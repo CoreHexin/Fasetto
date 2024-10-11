018173e84b391cacc828ce134df9460aaae7ecaf
Hexin <hexin.email@qq.com>
2024-10-11 14:43:03 +0800

    自定义数据模板选择器

---
+++ Fasetto/TemplateSelectors/ChatMessageListItemDataTemplateSelector.cs
@@ -0,0 +1,22 @@
+﻿using Fasetto.ViewModels;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Fasetto.TemplateSelectors;
+
+/// <summary>
+/// 数据模板选择器
+/// </summary>
+public class ChatMessageListItemDataTemplateSelector : DataTemplateSelector
+{
+    public DataTemplate ChatMessageListItemDataTemplate { get; set; }
+    public DataTemplate ChatMessageListItemSentByMeDataTemplate { get; set; }
+
+    public override DataTemplate SelectTemplate(object item, DependencyObject container)
+    {
+        if (item is ChatMessageListItemViewModel vm && vm.IsSentByMe == true)
+            return ChatMessageListItemSentByMeDataTemplate;
+
+        return ChatMessageListItemDataTemplate;
+    }
+}
