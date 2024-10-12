2b28c04b028a5be624293afaa6f72afdf71f5090
Hexin <hexin.email@qq.com>
2024-10-12 10:07:20 +0800

    通过附加属性给文本输入框获取焦点

---
+++ Fasetto/AttachedProperties/TextBoxAttachedProperties.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Fasetto.AttachedProperties;
+
+public class TextBoxAttachedProperties
+{
+    public static readonly DependencyProperty IsFocusedProperty = DependencyProperty.RegisterAttached(
+        "IsFocused", typeof(bool), typeof(TextBoxAttachedProperties), new PropertyMetadata(OnPropertyChanged));
+
+    private static void OnPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is Control control)
+        {
+            control.Focus();
+        }
+    }
+
+    public static bool GetIsFocused(DependencyObject obj)
+    {
+        return (bool)obj.GetValue(IsFocusedProperty);
+    }
+
+    public static void SetIsFocused(DependencyObject obj, bool value)
+    {
+        obj.SetValue(IsFocusedProperty, value);
+    }
+
+}
