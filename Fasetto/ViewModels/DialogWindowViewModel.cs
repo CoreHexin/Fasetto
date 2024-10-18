278bf951456d5bdc4f42724426356211fa65b2a4
Hexin <hexin.email@qq.com>
2024-10-18 12:56:57 +0800

    新增自定义对话框

---
+++ Fasetto/ViewModels/DialogWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Windows.Controls;
+
+namespace Fasetto.ViewModels;
+
+public partial class DialogWindowViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string _title = string.Empty;
+
+    [ObservableProperty]
+    private int _windowCornerRadius = 10;
+
+    [ObservableProperty]
+    private string _message = string.Empty;
+
+    [ObservableProperty]
+    private Control? _content;
+
+    public DialogWindowViewModel()
+    {
+    }
+
+}
---
+++ Fasetto/Views/DialogWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Fasetto.ViewModels;
+using System.Windows;
+
+namespace Fasetto.Views
+{
+    /// <summary>
+    /// DialogWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class DialogWindow : Window
+    {
+        public DialogWindow()
+        {
+            InitializeComponent();
+            DataContext = new DialogWindowViewModel();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
